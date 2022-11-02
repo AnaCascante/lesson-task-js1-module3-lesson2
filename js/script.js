76e1bbeaedeee9f3e948414ec10c0179675228d8
AnaCascante <ana.cascante@stud.noroff.no>
2022-11-02 09:20:15 +0100

    done

--- js/script.js
+++ js/script.js
@@ -8,23 +8,27 @@ function greet(name = "John") {
 
 greet (); 
 
-### Question 2
+// Question 2  Create a reusable function that accepts two parameters, then returns a dynamic HTML `<div>` element.  The `<div>` it creates should insert one of the parameters as a class, and the other as the inner value. Similar to the below HTML:
 
-Create a reusable function that accepts two parameters, then returns a dynamic HTML `<div>` element.  
-The `<div>` it creates should insert one of the parameters as a class, and the other as the inner value. Similar to the below HTML:
-
-```html
+/*```html
 <div class="class">value</div>
-```
+```*/
 
-### Question 3:
-
-Convert the below API call to use a try-catch-finally statement.
-
-```js
-async function getFact() {
-    const response = await fetch("https://cat-fact.herokuapp.com/facts");
-    const results = await response.json();
-    console.log(results[8].text);
+function createMessage (class, value){
+    const html = `<div class="class ${class}">${value}</div>`;
+    return html;
 }
-```
+
+// Question 3:  Convert the below API call to use a try-catch-finally statement.
+
+async function getFact() {
+    try {
+        const response = await fetch("https://cat-fact.herokuapp.com/facts");
+        const results = await response.json();
+        console.log(results[8].text);
+    }
+    catch(error){
+        console.log (error)
+    }
+}
+
